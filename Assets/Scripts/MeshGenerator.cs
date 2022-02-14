736dd9eb0353fedd0cee597770916c4934d972bd
detonator22 <ashutoshinde23@gmail.com>
2022-02-14 16:34:23 +0530

    Sphere Mesh generation

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -5,88 +5,154 @@ using UnityEngine;
 public class MeshGenerator : MonoBehaviour
 {
     //Serialized vars
-    [SerializeField] int height, width;
-    [SerializeField] float pnAmplititude;
-    [SerializeField] Vector2 pnScale;
-
+    [SerializeField] int n_latitudes, n_longitudes;
+    [SerializeField] float radius;
     Mesh mesh;
+
 
     List<Vector3> vertices;
     List<int> triangles;
+
+
+    // non-state vars
+    float PI = Mathf.PI;
     void Start()
     {
         mesh = new Mesh();
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
         GetComponent<MeshFilter>().mesh = mesh;
 
         vertices = new List<Vector3>();
         triangles = new List<int>();
 
-        
         CreateMesh();
-        Terrainize();
         UpdateMesh();
     }
 
-
-    //creates a plane
     void CreateMesh()
     {
-        // add vertices
-        for(int i = 0; i < height; i++)
+        vertices.Clear();
+        triangles.Clear();
+        // uv sphere / disco ball
+        // draw middle loops
+        // each loop contains n_longitudes vertices
+        // there are total n_latitudes loops
+
+        // top point
+        vertices.Add(new Vector3(0, radius, 0));
+
+        // middle loops
+        for (int h = 0; h < n_latitudes; h++)
         {
-            for(int j = 0; j < width; j++)
+            float angle1 = (h + 1) * PI / (n_latitudes + 1);
+            for (int v = 0; v < n_longitudes; v++)
             {
-                vertices.Add(new Vector3(j, 0, i));
+                float angle2 = v * PI * 2 / n_longitudes;
+
+                float x = Mathf.Sin(angle1) * Mathf.Cos(angle2);
+                float y = Mathf.Cos(angle1);
+                float z = Mathf.Sin(angle1) * Mathf.Sin(angle2);
+                Vector3 pointOnSphere = (new Vector3(x, y, z)) * radius;
+                vertices.Add(pointOnSphere);
             }
         }
 
-        //add triangles
-        for(int i = 0; i < height - 1; i++)
-        {
-            for(int j = 0; j < width - 1; j++)
-            {
-                int i_currentVertex = i * (width) + j;
-                
-                triangles.Add(i_currentVertex + 1 + width);
-                triangles.Add(i_currentVertex + 1);
-                triangles.Add(i_currentVertex);
+        // bottom point
+        vertices.Add(new Vector3(0, -radius, 0));
 
-                triangles.Add(i_currentVertex);
-                triangles.Add(i_currentVertex + width);
-                triangles.Add(i_currentVertex + 1 + width);
+        // add triangles
+
+        // top cap
+        for(int i = 0; i < n_longitudes; i++)
+        {
+            int index = 0 * n_latitudes + i + 1;
+            if(i == n_longitudes - 1)
+            {
+                triangles.Add(index);
+                triangles.Add(0);
+                triangles.Add(1);
+            }
+            else
+            {
+                triangles.Add(index);
+                triangles.Add(0);
+                triangles.Add(index + 1);
+            }
+        }
+
+        // add middle triangles
+        for (int la = 0; la < n_latitudes - 1; la++)
+        {
+            for (int lo = 0; lo < n_longitudes; lo++)
+            {
+                int index = la * n_longitudes + lo + 1;
+                if (lo == n_longitudes - 1)
+                {
+                    //final quad
+                    triangles.Add(index);
+                    triangles.Add(index - n_longitudes + 1);
+                    triangles.Add(index + 1);
+
+                    triangles.Add(index + n_longitudes);
+                    triangles.Add(index);
+                    triangles.Add(index + 1);
+                }
+                else
+                {
+                    //other quads
+                    triangles.Add(index);
+                    triangles.Add(index + 1);
+                    triangles.Add(index + n_longitudes + 1);
+
+                    triangles.Add(index + n_longitudes);
+                    triangles.Add(index);
+                    triangles.Add(index + n_longitudes + 1);
+                }
+            }
+        }
+    
+        // bottom cap
+        for(int i = 0; i < n_longitudes; i++)
+        {
+            int index = (n_latitudes - 1) * n_longitudes + i + 1;
+            if(i == n_longitudes - 1)
+            {
+                //last triangle
+                triangles.Add(index);
+                triangles.Add(index - n_longitudes + 1);
+                triangles.Add(0);
+            }
+            else
+            {
+                triangles.Add(index);
+                triangles.Add(index + 1);
+                triangles.Add(0);
             }
         }
     }
-
     void UpdateMesh()
     {
         mesh.Clear();
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
     }
 
-    int IndexOfVertex(int x, int z)
+    private void OnDrawGizmos()
     {
-        return z * width + x;
-    }
-
-    bool isInBounds(int x, int z)
-    {
-        //check if the index value is in bounds of the vertex array
-        if (x < 0 || z < 0 || x >= width || z >= height) return false;
-        else return true;
-    }
-
-    void Terrainize()
-    {
-        for(int z = 0; z < height; z++)
+        if (vertices == null) return;
+        for (int i = 0; i < vertices.Count; i++)
         {
-            for(int x = 0; x < width; x++)
-            {
-                int index = IndexOfVertex(x, z);
-                float y = Mathf.PerlinNoise(x * pnScale.x, z * pnScale.y) * pnAmplititude;
-                vertices[index] = new Vector3(vertices[index].x, y, vertices[index].z);
-            }
+            Gizmos.DrawSphere(vertices[i], 0.02f);
         }
+
+        //if (triangles == null) return;
+        //for (int i = 0; i < triangles.Count; i = i + 3)
+        //{
+        //    Gizmos.DrawLine(vertices[triangles[i]], vertices[triangles[i + 1]]);
+        //    Gizmos.DrawLine(vertices[triangles[i + 1]], vertices[triangles[i + 2]]);
+        //    Gizmos.DrawLine(vertices[triangles[i]], vertices[triangles[i + 2]]);
+        //}
     }
+
 }
